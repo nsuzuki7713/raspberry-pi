65abcd4d3c2f69e26e5ef7820482c011e8bc8d4d
nsuzuki7713 <nsuzuki7713@gmail.com>
2021-06-27 20:23:25 +0900

    node-cronを設定

--- packages/networkSpeed/src/index.ts
+++ packages/networkSpeed/src/index.ts
@@ -1,11 +1,13 @@
 import { getNetworkDownloadSpeed, getNetworkUploadSpeed } from "./networkSpeed";
 import { insertNetworkSpeed } from "./spreadsheet";
+import cron from "node-cron"
 
-(async () => {
+
+cron.schedule('*/15 * * * *', async () => {
   const [downloadSpeed, uploadSpeed] = await Promise.all([
     getNetworkDownloadSpeed(),
     getNetworkUploadSpeed(),
   ]);
 
   await insertNetworkSpeed(downloadSpeed, uploadSpeed);
-})().catch((e) => console.log(e));
+});
